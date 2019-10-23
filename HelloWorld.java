64ccffba50fa91918a49075d12bd847d28322cc6
GuDwade <849467768@qq.com>
2019-10-23 22:43:19 +0800

    提交代码

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello everyone");
+		System.out.println("我是顾富成");
+		System.out.println("我会好好学习Java的");
+	}
+}
