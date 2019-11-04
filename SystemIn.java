9f21bd60974004ce8d5bdd8e856a545887e1cc02
GuDwade <849467768@qq.com>
2019-11-04 23:10:36 +0800

    提交代码

---
+++ SystemIn.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class SystemIn{
+	public static void main(String[]args){
+		Scanner scanner=new Scanner(System.in);
+		System.out.println("请输入");
+		// int a=scanner .nextInt();
+		// System.out.println(a);
+		// float b=scanner.nextFloat();
+		// System.out.println(b);
+		// double c=scanner.nextDouble();
+		// System.out.println(c);
+		// char  e=scanner.next().charAt(0);
+		// System.out.println(e);
+		String d=scanner.next();
+		System.out.println(d);
+		String D=scanner.nextLine();
+		System.out.println(D);
+	}
+}
