5d06cfec9443dd0f8d533775c840ffd49a6a4ebc
GuDwade <849467768@qq.com>
2019-11-23 09:16:38 +0800

    提交代码

---
+++ start/FileCopy.java
@@ -0,0 +1,22 @@
+import java.io.*;
+public class FileCopy{
+	public static void main(String[]args){
+		// try{
+			// FileReader input=new FileReader("FileCopy.java");
+			// FileWriter output=new FileWriter("E:\\gufucheng.txt");
+		    // int c=input.read();
+			// while(c!=-1){
+				// output.write(c);
+				// System.out.println((char)c);
+				// c=input.read();
+			// }
+			// input.close();
+			// output.close();
+		// }catch(IOException e){
+			// System.out.println(e);
+		// }	
+		try{
+			FileReader input=new FileReader("FileCopy.java");
+		}
+	}
+}		
