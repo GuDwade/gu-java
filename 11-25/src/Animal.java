2282b4aeebe658e7a0d02c20be3fd986cbbdd181
GuDwade <849467768@qq.com>
2019-12-15 17:15:24 +0800

    提交代码

---
+++ 11-25/src/Animal.java
@@ -0,0 +1,15 @@
+public class Animal {
+    int color;
+    int numberOfLegs;
+    Animal(){
+        color=0;
+        numberOfLegs=1;
+    }
+    public void hell() {
+        System.out.println("我有" + numberOfLegs + "条腿");
+    }
+    public void scream(){
+        System.out.println("我哪知道？");
+    }
+
+}
