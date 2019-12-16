8997897978c78f6f61e0d6d27edb997b54e86a25
GuDwade <849467768@qq.com>
2019-12-16 22:43:52 +0800

    客户端

---
+++ 2019-12-16/src/TestClient.java
@@ -0,0 +1,20 @@
+import jdk.internal.util.xml.impl.Input;
+
+import java.net.*;
+import java.io.*;
+
+public class TestClient {
+    public static void main(String[] args) {
+        try{
+        Socket s1=new Socket("127.0.0.1",8888);
+        InputStream is=s1.getInputStream();
+        DataInputStream dis=new DataInputStream(is);
+            System.out.println(dis.readUTF());
+            dis.close();
+            s1.close();
+    }catch(ConnectException connExc){
+            System.out.println("连接失败");
+        }catch (IOException e){
+        }
+    }
+}
