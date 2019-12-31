51e669d74d6bb7740a86b88441edaead23197300
GuDwade <849467768@qq.com>
2019-12-31 16:20:20 +0800

    提交代码

---
+++ 2019-12-30字符串/src/MyString.java
@@ -0,0 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public  final class MyString {
+    private  static List<MyString> constantPool=new ArrayList<>();
+    private  final char[] value;
+    private final int length;
+
+    public MyString(char[] value) {
+        this.value = Arrays.copyOf(value,value.length);
+        this.length=value.length;
+    }
+    public  MyString contact(MyString s){
+        char[]val=new char[length+s.length];
+        System.arraycopy(value,0,val,0,length);
+        System.arraycopy(s.value,0,val,length,s.length);
+        return new MyString(val);
+    }
+    public char[] toCharArray(){
+        return Arrays.copyOf(value,length);
+    }
+    public MyString intern(){
+        for(MyString s:constantPool){
+            if(s.equals(this)){
+                return s;
+            }
+        }
+        constantPool.add(this);
+        return this;
+    }
+} 
