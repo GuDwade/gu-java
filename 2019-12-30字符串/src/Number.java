7d322363387fedc264197834c20e32e163fdab53
GuDwade <849467768@qq.com>
2020-01-05 23:26:10 +0800

    提交代码

---
+++ 2019-12-30字符串/src/Number.java
@@ -0,0 +1,25 @@
+public class Number {
+    private static int[] nums = new int[] {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+
+    private static String[] strings = new String[] {
+            "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"
+    };
+
+    public static String intToRoman(int num) {
+        StringBuilder sb = new StringBuilder();
+        int i = 0;
+        while (num > 0) {
+            if (num - nums[i] >= 0) {
+                sb.append(strings[i]);
+                num -= nums[i];
+            } else {
+                i++;
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(intToRoman(5));
+    }
+}
