e9674e08c2046518a7f36314859f20874b8e27bb
GuDwade <849467768@qq.com>
2020-01-19 19:33:50 +0800

    提交代码

--- 2019-12-29链表/src/ListNode.java
+++ 2019-12-29链表/src/ListNode.java
@@ -5,4 +5,16 @@ public class ListNode {
     ListNode(int val) {
         this.val = val;
     }
+
+    public  static  void print(ListNode head){
+        if(head==null){
+            return;
+        }
+        ListNode cur=head;
+        while(cur!=null){
+            System.out.printf("%s\t",cur.val);
+            cur=cur.next;
+        }
+        System.out.println();
+    }
 }
