e2ec659125d394e68e63a94aa39e24dddb35ded9
GuDwade <849467768@qq.com>
2020-02-11 22:47:51 +0800

    最小栈

---
+++ 2020-2-6栈/src/MinStack.java
@@ -0,0 +1,74 @@
+import java.util.Stack;
+
+public class MinStack {
+    /*
+    Stack <Integer>stack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        stack=new Stack();
+        minStack=new Stack();
+    }
+
+    public void push(int x) {
+        if(minStack.isEmpty()|| x<= minStack.peek()){
+            minStack.push(x);
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        int ret=stack.peek();
+        if(ret==minStack.peek()){
+            minStack.pop();
+        }
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return  minStack.peek();
+    }
+    */
+    int min;
+    Stack <Integer>stack;
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack=new Stack();
+        min=Integer.MAX_VALUE;
+    }
+
+    public void push(int x) {
+        if(stack.isEmpty()){
+            min=x;
+        }
+        if(x<=min){
+            stack.push(min);
+            min=x;
+        }
+        stack.push(x);
+    }
+
+    public void pop() {
+        if(stack.pop()==min){
+            min=stack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+    public static void main(String[] args) {
+        MinStack min=new MinStack();
+        min.push(0);
+        min.push(3);
+        min.push(-1);
+        System.out.println(min.min);
+        min.pop();
+        System.out.println(min.min);
+    }
+}
+
