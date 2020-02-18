31ff65d798dd02742fedd315f405d10c968a42db
GuDwade <849467768@qq.com>
2020-02-18 22:15:18 +0800

    下一个更大的元素

---
+++ 2020-2-6栈/src/MinNumber.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class MinNumber {
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Stack<Integer>stack=new Stack<>();
+        Map<Integer,Integer>map=new HashMap<>();
+        for(int i=0;i<nums2.length;i++){
+            while(!stack.isEmpty()&&nums2[i]>stack.peek()){
+                map.put(stack.pop(),nums2[i]);
+            }
+            stack.push(nums2[i]);
+        }
+        while(!stack.isEmpty()){
+            map.put(stack.pop(),-1);
+        }
+        int []ret=new int[nums1.length];
+        for(int i=0;i<nums1.length;i++){
+            ret[i]=map.get(nums1[i]);
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        int[] nums1={2,4,1};
+        int[]nums2={1,4,3,2};
+        System.out.println(Arrays.toString(nextGreaterElement(nums1,nums2)));
+    }
+}
