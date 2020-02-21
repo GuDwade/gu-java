2b9d71737daf1bfc3e731b9f495dc6f1c7c7beeb
GuDwade <849467768@qq.com>
2020-02-21 19:35:47 +0800

    二叉搜索树返回双向链表 根据前序中序构建二叉树

---
+++ 2020-2-17二叉树/src/BinaryTreeHardPro.java
@@ -0,0 +1,56 @@
+public class BinaryTreeHardPro {
+    public static TreeNode prev=null;
+    //中序遍历将二叉搜索树排序 改变链表方向
+    public void _Convert(TreeNode root) {
+        if(root==null){
+            return;
+        }
+        _Convert(root.left);
+        root.left=prev;
+        if(prev!=null){
+            prev.right=root;
+        }
+        prev=root;
+        _Convert(root.right);
+    }
+
+    public TreeNode Convert(TreeNode pRootOfTree) {
+        if(pRootOfTree==null){
+            return null;
+        }
+        //重置 否则与上一次测试的树相连接
+        prev=null;
+        _Convert(pRootOfTree);
+        //返回排序后的头节点 即左边为空的结点
+        while( pRootOfTree.left!=null){
+            pRootOfTree= pRootOfTree.left;
+        }
+        return  pRootOfTree;
+    }
+    public static int curIndex=0;
+    public TreeNode _buildTree(int[] preorder, int[] inorder,int inStart,int inEnd) {
+        if(inStart>inEnd){
+            return null;
+        }
+        int rootVal=preorder[curIndex++];
+        TreeNode root=new TreeNode(rootVal);
+        if(inStart==inEnd){
+            return root;
+        }
+        int i;
+        for(i=inStart;i<=inEnd;i++){
+            if(rootVal==inorder[i]){
+                break;
+            }
+        }
+        root.left=_buildTree(preorder,inorder,inStart,i-1);
+        root.right=_buildTree(preorder,inorder,i+1,inEnd);
+        return root;
+    }
+    //根据前序和中序构建二叉树
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        curIndex=0;
+        return _buildTree(preorder,inorder,0,inorder.length-1);
+    }
+
+}
