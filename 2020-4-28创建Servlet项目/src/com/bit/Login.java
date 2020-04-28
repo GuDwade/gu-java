12b5a45462983c12a5365399f0d7bf63da9c182b
GuDwade <849467768@qq.com>
2020-04-28 22:12:10 +0800

    动态资源

---
+++ 2020-4-28创建Servlet项目/src/com/bit/Login.java
@@ -0,0 +1,32 @@
+package com.bit;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Login extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       String username=req.getParameter("username");
+       String password=req.getParameter("password");
+       resp.setContentType("text/plain; charset=utf-8");
+       PrintWriter writer=resp.getWriter();
+       if(username!=null
+               &&username.equals("gufucheng")
+               &&password!=null
+               &&password.equals("123456")){
+
+           writer.println("登录成功");
+           System.out.println("登录成功");
+
+       }else{
+           writer.println("登录失败");
+           System.out.println("登录失败");
+       }
+    }
+}
