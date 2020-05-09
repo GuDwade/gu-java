1dc3fbe31c72246d2c35ca93b29403097e316681
GuDwade <849467768@qq.com>
2020-05-09 21:26:57 +0800

    平方根

---
+++ one problem every day/src/May/Day9.java
@@ -0,0 +1,22 @@
+package May;
+
+public class Day9 {
+
+    //平方根
+
+    public int mySqrt(int x) {
+        int left=0;
+        int right=x;
+
+        while(left<right){
+            int mid=left+right+1>>1;
+            if(mid<=x/mid){
+                left=mid;
+            }else{
+                right=mid-1;
+            }
+        }
+
+        return left;
+    }
+}
