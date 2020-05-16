2958c556146e67593e3603b904af8fbdcd835092
GuDwade <849467768@qq.com>
2020-05-16 21:51:05 +0800

    只出现一次的数

---
+++ one problem every day/src/May/Day14.java
@@ -0,0 +1,21 @@
+package May;
+
+public class Day14 {
+    //只出现一次的数字 I II III
+
+    //a^a=0;  a^0=a;
+    public int singleNumber1(int[] nums) {
+        int singleNumber=0;
+        for(int num:nums){
+            singleNumber^=num;
+        }
+        return singleNumber;
+    }
+
+
+    //除了一个数出现一次以外其余每个数都出现3次
+    public int singleNumber2(int[] nums) {
+
+
+    }
+}
