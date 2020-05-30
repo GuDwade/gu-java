236ac5011a1ef3dc7d00b88164106e8420fc7fec
GuDwade <849467768@qq.com>
2020-05-30 23:22:21 +0800

    柱状图中最大的矩形

---
+++ one problem every day/src/May/Day27.java
@@ -0,0 +1,23 @@
+package May;
+
+public class Day27 {
+    // 柱状图中最大的矩形
+    public int largestRectangleArea(int[] heights) {
+        int area = 0, n = heights.length;
+        // 遍历每个柱子，以当前柱子的高度作为矩形的高 h
+        // 从当前柱子向左右遍历，找到矩形的宽度 w。
+        for (int i = 0; i < n; i++) {
+            int w = 1, h = heights[i], j = i;
+            while (--j >= 0 && heights[j] >= h) {
+                w++;
+            }
+            j = i;
+            while (++j < n && heights[j] >= h) {
+                w++;
+            }
+            area = Math.max(area, w * h);
+        }
+
+        return area;
+    }
+}
