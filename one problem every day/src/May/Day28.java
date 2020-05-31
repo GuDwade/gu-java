87305de33ad1f0ae0588bb970cff1867e0b7191f
GuDwade <849467768@qq.com>
2020-05-31 23:05:23 +0800

    对称二叉树

---
+++ one problem every day/src/May/Day28.java
@@ -0,0 +1,53 @@
+package May;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Day28 {
+    //对称二叉树
+    public boolean isSymmetric(TreeNode root) {
+        if (root==null){
+            return  true;
+        }
+
+        return isSame(root.left,root.right);
+
+    }
+
+    public boolean isSame(TreeNode node1,TreeNode node2){
+        if(node1==null||node2==null){
+            return  node1==null&&node2==null;
+        }
+
+        return  node1.val==node2.val
+                &&isSame(node1.left,node2.right)
+                &&isSame(node1.right,node2.left);
+    }
+
+    public boolean isSymmetric1(TreeNode root) {
+        if (root==null){
+            return  true;
+        }
+
+        Queue<TreeNode>queue=new LinkedList<>();
+        queue.offer(root.left);
+        queue.offer(root.right);
+
+        while (!queue.isEmpty()){
+            TreeNode node1=queue.poll();
+            TreeNode node2=queue.poll();
+            if (node1==null&&node2==null){
+                continue;
+            }
+            if (node1==null||node2==null||node1.val!=node2.val){
+                return  false;
+            }
+            queue.offer(node1.left);
+            queue.offer(node2.right);
+            queue.offer(node1.right);
+            queue.offer(node2.left);
+        }
+
+        return  true;
+    }
+}
