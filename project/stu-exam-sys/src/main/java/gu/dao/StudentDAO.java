99be0789e52044a13c67179f011833d9ef0bf25d
GuDwade <849467768@qq.com>
2020-08-23 22:52:40 +0800

    查询学生数据字典

---
+++ project/stu-exam-sys/src/main/java/gu/dao/StudentDAO.java
@@ -0,0 +1,39 @@
+package gu.dao;
+
+import gu.model.DictionaryTag;
+import gu.util.DBUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentDAO {
+
+    public static List<DictionaryTag> queryAsDict(int id) {
+        List<DictionaryTag> tags = new ArrayList<>();
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            c = DBUtil.getConnection();
+            String sql = "select id, student_name from student where classes_id=?";
+            ps = c.prepareStatement(sql);
+            ps.setInt(1, id);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                DictionaryTag tag=new DictionaryTag();
+                tag.setDictionaryTagKey(String.valueOf(rs.getInt("id")));
+                tag.setDictionaryTagValue(rs.getString("student_name"));
+                tags.add(tag);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("查询学生数据字典出错", e);
+        } finally {
+            DBUtil.close(c, ps, rs);
+        }
+        return tags;
+    }
+}
