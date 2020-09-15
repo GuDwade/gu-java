b68119a22c44e84414f4105a4399447ce5e85f01
GuDwade <849467768@qq.com>
2020-09-15 09:18:25 +0800

    发送ajax请求

---
+++ ques-sur/web/js/ajax.js
@@ -0,0 +1,50 @@
+"use strict";   
+
+function apiGet(url) {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open("get", url);
+        xhr.onload = function (ev) {
+            if (xhr.status !== 200) {
+                reject(xhr.responseText);  
+                return;
+            }
+
+            // { status: 200, data: ... }
+            let result = JSON.parse(xhr.responseText);
+            if (result.status !== 200) {   
+                reject(result.reason);  
+            } else {
+                resolve(result.data);   
+            }
+        };
+        xhr.onerror = function (ev) {
+            reject(xhr.responseText);   
+        };
+        xhr.send();
+    });
+}
+
+function apiPost(url, data) {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open("post", url);
+        xhr.setRequestHeader("Content-Type", "application/json; charset=utf-8");
+        xhr.onload = function (ev) {
+            if (xhr.status !== 200) {
+                reject(xhr.responseText);  
+                return;
+            }
+            let result = JSON.parse(xhr.responseText);
+            if (result.status !== 200) {    
+                reject(result.reason);  
+            } else {
+                resolve(result.data);  
+            }
+        };
+        xhr.onerror = function (ev) {
+            reject(xhr.responseText);  
+        };
+        xhr.send(JSON.stringify(data));
+    });
+}
