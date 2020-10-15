20ab1b84fe50e55d374685ca9da895d89266e5cf
GuDwade <849467768@qq.com>
2020-10-15 16:04:37 +0800

    填充每个节点的下一个右侧节点指针

---
+++ one problem every day/src/Dec/Day15.java
@@ -0,0 +1,61 @@
+package Dec;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+}
+
+
+public class Day15 {
+
+    //填充每个节点的下一个右侧节点指针
+
+    public Node connect(Node root) {
+        if(root==null){
+            return  root;
+        }
+
+        Queue<Node>queue=new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()){
+            int sz=queue.size();
+            while (sz>0){
+                Node cur=queue.poll();
+                sz--;
+                if (sz>0){
+                    cur.next=queue.peek();
+                }
+
+                if (cur.left!=null){
+                    queue.add(cur.left);
+                }
+
+                if (cur.right!=null){
+                    queue.add(cur.right);
+                }
+            }
+        }
+
+        return  root;
+    }
+
+}
