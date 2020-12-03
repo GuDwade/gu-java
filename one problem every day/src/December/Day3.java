87b0d61de9a03749b24f66dc3d75e0b773c6ce8f
GuDwade <849467768@qq.com>
2020-12-03 22:37:23 +0800

    计数质数

---
+++ one problem every day/src/December/Day3.java
@@ -0,0 +1,27 @@
+package December;
+
+import java.util.Arrays;
+
+public class Day3 {
+
+    // 计数质数
+    public int countPrimes(int n) {
+        boolean[] isPrim = new boolean[n];
+        Arrays.fill(isPrim, true);
+        for (int i = 2; i * i < n; i++) {
+            if (isPrim[i]) {
+                for (int j = i * i; j < n; j += i) {
+                    isPrim[j] = false;
+                }
+            }
+        }
+
+        int cnt = 0;
+        for (int i = 2; i < n; i++) {
+            if (isPrim[i]) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
