192548a1526dcccb53b2815c3c04a0c35697633a
GuDwade <849467768@qq.com>
2020-12-15 23:28:35 +0800

    单调递增的数字

---
+++ one problem every day/src/December/Day15.java
@@ -0,0 +1,29 @@
+package December;
+
+public class Day15 {
+    //单调递增的数字
+    public static int monotoneIncreasingDigits(int N) {
+       char[] arr = (N +"").toCharArray();
+       int max = -1, idx = -1;
+       for(int i = 0; i  < arr.length - 1; i++) {
+           if (max < arr[i]){
+               max = arr[i];
+               idx = i;
+           }
+
+           if (arr[i] > arr[i + 1]){
+               arr[idx] -= 1;
+               for(int j = idx + 1; j < arr.length; j++){
+                   arr[j] = '9';
+               }
+               break;
+           }
+       }
+
+       return  Integer.parseInt(new String(arr));
+    }
+
+    public static void main(String[] args) {
+        System.out.println(monotoneIncreasingDigits(332));
+    }
+}
