3c10751b6621f6b3a5f0b45b71cb0a8959732b36
gucheng <849467768@qq.com>
2021-03-09 22:55:57 +0800

    机器人的运动范围

---
+++ 剑指/src/机器人的运动范围.java
@@ -0,0 +1,53 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class 机器人的运动范围 {
+
+    int[] dx = {0,1};
+    int[] dy = {1,0};
+
+    public int movingCount(int m, int n, int k) {
+        if (k == 0){
+            return  1;
+        }
+
+        Queue<int[]>queue = new LinkedList<>();
+        boolean[][]visted = new boolean[m][n];
+        visted[0][0] = true;
+        queue.offer(new int[]{0,0});
+        int ret = 1;
+
+        while (!queue.isEmpty()){
+            int[] plot = queue.poll();
+            for(int i = 0; i < 2;i++ ){
+                int newX = plot[0] + dx[i];
+                int newY = plot[1] + dy[i];
+
+                if (newX >= m || newY >= n || getSum(newX) + getSum(newY) > k || visted[newX][newY]){
+                    continue;
+                }
+                queue.offer(new int[]{newX,newY});
+                visted[newX][newY] = true;
+                ret++;
+            }
+        }
+
+        return  ret;
+
+    }
+
+    private int getSum(int sum) {
+        int ret = 0;
+        while (sum != 0){
+            ret += sum % 10;
+            sum /= 10;
+        }
+        return  ret;
+    }
+
+    public static void main(String[] args) {
+        机器人的运动范围  test = new 机器人的运动范围();
+        test.movingCount(1,2,1);
+
+    }
+}
